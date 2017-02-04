3e294dbb2f66fb24431ff9f0c84fad0d82364673
Matt Gowie <gowie.matt@gmail.com>
2017-02-04 16:18:10 -0500

    Adds httpoison + first working cut

--- lib/where_am_i.ex
+++ lib/where_am_i.ex
@@ -1,18 +1,20 @@
 defmodule WhereAmI do
   @moduledoc """
-  Documentation for WhereAmI.
+  Command line application to POST location informaation to the WhereAmI Server
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> WhereAmI.hello
-      :world
 
   """
-  def hello do
-    :world
+  def main(_) do
+    ip_addr = case HTTPoison.get("icanhazip.com") do
+      {:ok, %HTTPoison.Response{body: body}} -> String.trim(body)
+      {_, _} -> nil
+    end
+
+    location = IP2Location.lookup(String.trim(ip_addr))
+    post_body = Map.take(location, [:county_long, :city, :region, :latitude, :longitude, :ip])
+
+    HTTPoison.post "https://whereami.mattgowie.com", post_body, [{"Content-Type", "application/json"}]
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,19 +7,17 @@ defmodule WhereAmI.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: WhereAmI],
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [application: [:httpoison, :ip2location], extra_applications: [:logger]]
   end
 
   defp deps do
     [
+      {:httpoison, "~> 0.10.0"},
       {:ip2location, github: "nazipov/ip2location-elixir"},
     ]
   end
