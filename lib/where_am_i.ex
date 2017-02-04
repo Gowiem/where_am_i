93aa4b7b4cf63d403160e97244f990c3113c0770
Matt Gowie <gowie.matt@gmail.com>
2017-02-04 00:19:26 -0500

    Initial commit of new Mix app

---
+++ lib/where_am_i.ex
@@ -0,0 +1,18 @@
+defmodule WhereAmI do
+  @moduledoc """
+  Documentation for WhereAmI.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WhereAmI.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/where_am_i_test.exs
@@ -0,0 +1,8 @@
+defmodule WhereAmITest do
+  use ExUnit.Case
+  doctest WhereAmI
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
