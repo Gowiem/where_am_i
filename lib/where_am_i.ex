24e8ff4b4dd44202e340d05eb4e778df0be47522
Matt Gowie <gowie.matt@gmail.com>
2017-02-04 16:52:56 -0500

    Removes unneeded String.trim + adds Doc string

--- lib/where_am_i.ex
+++ lib/where_am_i.ex
@@ -4,7 +4,11 @@ defmodule WhereAmI do
   """
 
   @doc """
+  Uploads the user's approximate location to whereami.mattgowie.com
 
+  1. Fetches the client's IP Address from icanhazip.com
+  2. Does a lookup of the fetched IP Address from the IP2Location database
+  3. POSTs the IP2Location.Result infomration to whereami.mattgowie.com
   """
   def main(_) do
     ip_addr = case HTTPoison.get("icanhazip.com") do
@@ -12,7 +16,7 @@ defmodule WhereAmI do
       {_, _} -> nil
     end
 
-    location = IP2Location.lookup(String.trim(ip_addr))
+    location = IP2Location.lookup(ip_addr)
     post_body = Map.take(location, [:county_long, :city, :region, :latitude, :longitude, :ip])
 
     HTTPoison.post "https://whereami.mattgowie.com", post_body, [{"Content-Type", "application/json"}]
