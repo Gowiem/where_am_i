ce57ff61c26168bd961fb31e817b3ba9dca117d4
Matt Gowie <gowie.matt@gmail.com>
2017-02-07 14:54:17 -0500

    Adds config info for where_am_i_host

---
+++ config/dev.exs
@@ -0,0 +1,2 @@
+use Mix.Config
+config :where_am_i, where_am_i_com_host: "http://localhost:80"
---
+++ config/prod.exs
@@ -0,0 +1,2 @@
+use Mix.Config
+config :where_am_i, where_am_i_com_host: "https://whereami.mattgowie.com"
